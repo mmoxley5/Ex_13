2252fc47c6f032e9f64b2ad1670fcc099a2f915c
mmoxley5 <112512916+mmoxley5@users.noreply.github.com>
2023-04-26 13:06:01 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,68 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+public class App {
+    public static void main(String[] args) {
+        CheckingAccount account = new CheckingAccount(100);
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.submit(new AccountHolder("Wife", account));
+        executor.submit(new AccountHolder("Husband", account));
+
+    }
+}
+class CheckingAccount {
+    // create a fair mutex
+    private Semaphore permits = new Semaphore(2, true);
+    private int balance;
+
+    public CheckingAccount(int initialBalance)
+    {
+        balance = initialBalance;
+    }
+
+    public synchronized int withdraw(int amount)
+    {
+        try {
+            permits.acquire();
+        } catch (InterruptedException e) {
+            // exception prevented acquiring a permit
+            return balance;
+        }
+        if (amount <= balance)
+        {
+            try {
+                Thread.sleep((int) (Math.random() * 200));
+            }
+            catch (InterruptedException ie) {
+            }
+
+            balance -= amount;
+        }
+
+        permits.release();
+        return balance;
+    }
+   
+}
+
+class AccountHolder implements Runnable {
+    private String name;
+    private CheckingAccount account;
+
+    AccountHolder(String name, CheckingAccount account) {
+        this.name = name;
+        this.account = account;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(name + " tries to withdraw $10, balance: " +
+                    account.withdraw(10));
+        }
+
+    }
+}
+
